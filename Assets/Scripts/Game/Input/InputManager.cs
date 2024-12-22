d67fe45e66aa6f1fff7bb2db7c547996ccaa9f9f
Ivan Horunov <ivan.horunov@elephant-games.am>
2024-12-22 17:34:47 +0300

    Init PC

--- Assets/Scripts/Game/Input/InputManager.cs
+++ Assets/Scripts/Game/Input/InputManager.cs
@@ -8,26 +8,46 @@ namespace Game.Input.Manager
     public class InputManager : MonoBehaviour, IInputManager
     {
         [SerializeField] private InputActionReference _swipeReference = default;
+        [SerializeField] private InputActionReference _clickReference = default;
 
         private float _xSwipe = 0f;
+
+        private bool _isLeftButtonClicked = false;
 
         public Action<float> XValueChanged { get; set; }
 
         public void Init()
         {
             _swipeReference.action.performed += ctx => CheckSwipe(ctx);
+            _clickReference.action.started += ctx => ButtonPressed(ctx);
+            _clickReference.action.canceled += ctx => ButtonRelised(ctx);
         }
 
         private void CheckSwipe(InputAction.CallbackContext ctx)
         {
-            Vector2 delta = ctx.ReadValue<Vector2>();
-            _xSwipe = delta.x;
-            XValueChanged?.Invoke(_xSwipe);
+            if (_isLeftButtonClicked)
+            {
+                Vector2 delta = ctx.ReadValue<Vector2>();
+                _xSwipe = delta.x;
+                XValueChanged?.Invoke(_xSwipe);
+            }
+        }
+
+        private void ButtonPressed(InputAction.CallbackContext ctx)
+        {
+            _isLeftButtonClicked = true;
+        }
+
+        private void ButtonRelised(InputAction.CallbackContext ctx)
+        {
+            _isLeftButtonClicked = false;
         }
 
         private void OnDestroy()
         {
             _swipeReference.action.performed -= ctx => CheckSwipe(ctx);
+            _clickReference.action.started -= ctx => ButtonPressed(ctx);
+            _clickReference.action.canceled -= ctx => ButtonRelised(ctx);
         }
     }
 
--- Assets/Scripts/UI/Windows/StartWindow.cs
+++ Assets/Scripts/UI/Windows/StartWindow.cs
@@ -1,13 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI.Windows
 {
     public class StartWindow : BaseWindow
     {
+        [SerializeField] private Image _logoImage = null;
         public Action GameStarted;
+
+        private Sequence _sequence = null;
+
+        private void OnEnable()
+        {
+            
+        }
 
         public void ScreenTucked()
         {
